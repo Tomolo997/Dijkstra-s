fe0ecd2f01da5d2f7def50b222f582a1cc30d2d1
Tomolo997 <tomazovsenjak7@gmail.com>
2021-01-13 15:15:46 +0100

    adedd the starting point and the ending point

--- app.js
+++ app.js
@@ -2,23 +2,34 @@
 const container = document.querySelector('.container');
 console.log(container);
 //make a grid
+const nodes = [];
 const COLS = 20;
 const ROWS = 20;
 function grid() {
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
       const div = document.createElement('div');
-      div.setAttribute('data-rows', i);
-      div.setAttribute('data-cols', j);
+      nodes.push({ name: div, row: i, col: j });
       div.classList.add('node');
       container.append(div);
     }
   }
+  console.log(nodes);
 }
-
-function setStartingPoint(rows, cols) {}
 
 function init() {
   grid();
 }
 init();
+
+function setStartingPoint(rows, cols) {
+  const startingNode = nodes.find((el) => el.row === rows && el.col === cols);
+  startingNode.name.classList.add('startingNode');
+}
+
+function setEndingPoint(rows, cols) {
+  const startingNode = nodes.find((el) => el.row === rows && el.col === cols);
+  startingNode.name.classList.add('endingNode');
+}
+setStartingPoint(12, 3);
+setEndingPoint(1, 12);
